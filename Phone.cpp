c0b79781d88993d2962ea065477ee488e7b11d94
makethecarnage <105937093+makethecarnage@users.noreply.github.com>
2023-07-03 15:10:29 +0300

    Create Phone.cpp

---
+++ Phone.cpp
@@ -0,0 +1,13 @@
+#include "Phone.h"
+
+void Phone::setYear(const unsigned int &year) {
+    this->year = year;
+}
+
+void Phone::setStorage(const unsigned int &storage) {
+    this->storage = storage;
+}
+
+void Phone::setPrice(const unsigned int &price) {
+    this->price = price;
+}
